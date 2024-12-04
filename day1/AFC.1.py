a38edd1d6249e7c870f2f71766b698b429909c08
kacpoks1 <abramowiczkapcer66@gmal.com>
2024-12-04 21:14:24 +0100

    "main"

---
+++ day1/AFC.1.py
@@ -0,0 +1,22 @@
+file = open("/home/kacper/Dokumenty/AdventOfCode/AFC.1.txt",'r')
+
+data = file.read()
+
+data = data.strip().split("\n")
+
+first = []
+second = []
+
+for line in data:
+    line = line.strip().split(" ")
+    first.append(line[0])
+    second.append(line[-1])
+
+
+score = 0
+
+for num in first:
+    score += int(num) * second.count(num)
+
+
+print(score)
